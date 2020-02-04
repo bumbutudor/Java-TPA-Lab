401fb989a8bfca385b48e9e1e5999221731027ba
bumbutudor <fcimia122@gmail.com>
2020-02-04 15:36:58 +0200

    re ous

---
+++ Sarcini/Lab 1/CharToStringDemo.java
@@ -0,0 +1,14 @@
+public class CharToStringDemo
+{
+   public static void main(String args[])
+   {
+      // Methoda 1: Folosind metoda toString() 
+      char ch = 'a';
+      String str = Character.toString(ch);
+      System.out.println("Stringul este: "+str);
+ 
+      // Methoda 2: Folosind metoda valueOf()
+      String str2 = String.valueOf(ch);
+      System.out.println("String is: "+str2);
+   }
+}
