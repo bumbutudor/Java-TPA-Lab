a39fd9750ed1ca509d03e460db7af4ae8f8be3fa
JulianNSH <JulianNSH@users.noreply.github.com>
2020-02-18 17:06:24 +0200

    LAB2 redactat
    A fost adaugata supraincarcarea metodelor Superclasei

--- IA-181/Paniș Iulian/LAB2/Stack.java
+++ IA-181/Paniș Iulian/LAB2/Stack.java
@@ -5,10 +5,26 @@
 class useTime{
     private int hour, minute, second;
     void setTime(){
-        hour = (int) (((System.currentTimeMillis() / 1000) / 60) / 60) % 24 + 3;
+        hour = (int) (((System.currentTimeMillis() / 1000) / 60) / 60) % 24 + 2;
         minute = (int) ((System.currentTimeMillis() / 1000) / 60) % 60;
         second = (int) (System.currentTimeMillis() / 1000) % 60;
     }
+    void setTime(int h){
+        hour = h;
+        minute = (int) ((System.currentTimeMillis() / 1000) / 60) % 60;
+        second = (int) (System.currentTimeMillis() / 1000) % 60;
+    }
+    void setTime(int h, int m){
+        hour = h;
+        minute = m;
+        second = (int) (System.currentTimeMillis() / 1000) % 60;
+    }
+    void setTime(int h, int m, int s){
+        hour = h;
+        minute = m;
+        second = s;
+    }
+
     void getTime(){
         System.out.println(hour+":"+minute+":"+second);
     }
@@ -34,9 +50,21 @@ class Stack extends useTime {
 
     public static void main(String[] args){
         Stack stiva = new Stack();
+        Stack stiva1 = new Stack();
+        Stack stiva2 = new Stack();
+        Stack stiva3 = new Stack();
 
         stiva.createStack();
         stiva.showStack();
+
+        stiva1.setTime(11);
+        stiva1.showStack();
+
+        stiva2.setTime(11, 22);
+        stiva2.showStack();
+
+        stiva3.setTime(10, 54, 9);
+        stiva3.showStack();
 
     }
 }
