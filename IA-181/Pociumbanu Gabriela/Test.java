71b046073e08d22806a4411781c1a8587bcd21b6
HP <pociumbanu_gabi@mail.ru>
2020-02-19 17:24:56 +0200

    Commit

---
+++ IA-181/Pociumbanu Gabriela/Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+    public static void main (String[]args)
+    {
+        Dreptunghi drept = new Dreptunghi (10,5);
+
+        Paralalepipid paral = new Paralalepipid (5,4,3);
+
+        System.out.println ("Volumul paralelipipedului "+paral.Volum ());
+
+    }
+}
