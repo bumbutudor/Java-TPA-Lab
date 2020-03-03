0b7f32aac5d42c72e0f04cb00fda4579c4f8259c
Petrachi Alexandru <42140204+BlackKakapo@users.noreply.github.com>
2020-03-03 20:17:38 +0200

    update lab2
    modificat lab2

--- IA-181/Petrachi Alexandru/lab2/lab2.java
+++ IA-181/Petrachi Alexandru/lab2/lab2.java
@@ -6,21 +6,40 @@ public class lab2 {
 
     static class Paragraf{
 
-        String numele;
+        String paragraf;
 
-        public Paragraf(String a){numele=a;}   //overloading
+
+        public Paragraf(String a){
+            paragraf =a;}   //overloading
         public Paragraf(){                  //overloading
 
             System.out.println("Introdu numele paragrafului:");
 
             Scanner s = new Scanner(System.in);
-            numele = s.nextLine();
+            paragraf = s.nextLine();
         }
 
         public void show(){                 //overwriting
-            System.out.println("Numele paragrafului este: " + numele);
+            System.out.println("Numele paragrafului este: " + paragraf);
         }
+        public void show(String cartea){System.out.println("Paragraful: " + paragraf + " din cartea: " + cartea);}
 
+        public void modify(){this.paragraf = "default";}
+        public void modify(String numele){this.paragraf = numele;}
+
+        public void hexDecimal(){
+
+            StringBuffer hex = new StringBuffer();
+            char cod[] = paragraf.toCharArray();
+
+            for (int i = 0;i < cod.length ; i++){
+                String hexString = Integer.toHexString(cod[i]);
+                hex.append(hexString);
+            }
+            paragraf = hex.toString();
+
+            System.out.println("Numele in hexDecimal: " + paragraf);
+        }
     }
 
     static class Pagini extends Paragraf{
@@ -30,23 +49,54 @@ public class lab2 {
         Pagini(String a, int b){super(a);pagini=b;}   //overloading
         Pagini(){                                   //overloading
             System.out.println("Introdu cate pagini are paragraful:");
-
             Scanner s = new Scanner(System.in);
             pagini = s.nextInt();
         }
 
-        public void show(){                         //overwriting
-            System.out.println("Numele paragrafului este "+ numele+" si are " + pagini + " pagini  ");
+        @Override
+        public void show(){System.out.println("Numele paragrafului este "+ paragraf +" si are " + pagini + " pagini  ");}
+        @Override
+        public void show(String cartea) {System.out.println("Paragraful: " + paragraf + " din cartea: " + cartea + " are " + pagini + " pagini");}
+
+        @Override
+        public void modify(){super.paragraf = "default"; this.pagini = 0;}
+        public void modify(String numele, int pagini) {super.paragraf = numele; this.pagini = pagini;}
+
+        @Override
+        public void hexDecimal(){
+            super.hexDecimal();
+            Integer b = pagini;
+            Integer.toHexString(b);
+            System.out.println("Paginile in foramt hexDecimal: " + b);
         }
     }
 
     public static void main(String[] args) {
         Paragraf p1 = new Paragraf();
+        Paragraf p2 = new Paragraf("Ziua trece");
         Pagini a1 = new Pagini();
         Pagini a2 = new Pagini("Marea dezolare",500);
 
         p1.show();
+        p1.modify();
+        p1.show();
+        p1.hexDecimal();
+
+        p2.show("Inima celor 3");
+        p2.modify("Nume schimbat");
+        p2.show();
+        p2.hexDecimal();
+
         a1.show();
+        a1.modify();
+        a1.show();
+        a1.hexDecimal();
+
         a2.show();
+        a2.modify("Nume schimbat",6);
+        a2.show("AUTOR");
+        a2.hexDecimal();
+
+
     }
 }
